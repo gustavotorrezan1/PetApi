e9723bb70b19ae2c492e2ee623d16f16a5efa209
gustavotorrezan1 <gustorrezani@gmail.com>
2024-01-08 17:05:18 -0300

    teste

--- Models/SubCategoria.cs
+++ Models/SubCategoria.cs
@@ -4,6 +4,5 @@ public class SubCategoria
     public int SubCategoriaId { get; set; }
     public int Nome { get; set; }
     public int Ativo { get; set; }
-    public int CategoriaId { get; set; }
     public Categoria? Categoria { get; set; }
 }
